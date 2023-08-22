9e3936f6c0d7523e1b219bb4d50621336065dd06
AnaliaInes <afrigerio30@gmail.com>
2023-08-21 21:26:26 -0300

    entrega del trabajo 21/8

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express= require("express");
+const app=express();
+const path=require ("path")
+
+
+app.use(express.static(path.join(__dirname,"../public")))
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,() =>{
+console.log("servidor cargando "+ PORT)}
+);
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+});
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
